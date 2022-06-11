b17de777b6f899e1eee0fbe2ef64eee370d1b351
gideonlangat98 <gideonlangat98@gmail.com>
2022-06-12 00:47:15 +0300

    commit tree 9b53a5eb972035c38c66c4384cbf287a4d408c2e

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+//appendCat destructively
+function destructivelyAppendCat() {
+cats.push("Ralph");
+}
+
+//prependCat destructively
+function destructivelyPrependCat() {
+cats.unshift("Bob");
+}
+//remove lastCat destructively
+function destructivelyRemoveLastCat() {
+cats.pop(2);
+}
+
+//remove firstCat destructively
+function destructivelyRemoveFirstCat() {
+cats.shift(0);
+}
+
+//appendCat to new array
+function appendCat(name) {
+let newCat;
+newCat = ([...cats, "Broom"]);
+return newCat;
+}
+
+
+//prependCat to new array
+function prependCat(name) {
+let newCat;
+newCat = (["Arnold", ...cats]);
+return newCat;
+}
+
+
+//removesLastCat and returns new array
+function removeLastCat() {
+let newArray;
+newArray = (["Milo", "Otis"]);
+newArray.splice(2, "Garfield");
+return newArray;
+}
+
+
+function removeFirstCat() {
+let newArray;
+newArray = (["Otis", "Garfield"]);
+newArray.splice(0, "Milo");
+return newArray;
+}
+
+
+
